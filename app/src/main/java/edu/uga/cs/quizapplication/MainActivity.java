9ecb19bf4ac009bada0f2682dc22052cd94b0cd5
tomking14 <tomykastor@gmail.com>
2023-10-23 12:56:45 -0400

    new addition test

--- app/src/main/java/edu/uga/cs/quizapplication/MainActivity.java
+++ app/src/main/java/edu/uga/cs/quizapplication/MainActivity.java
@@ -2,6 +2,7 @@ package edu.uga.cs.quizapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main);
+            // Load the landscape layout
+
+        } else {
+            // Load the portrait layout (activity_child.xml)
+            setContentView(R.layout.activity_main);
+        }
+
     }
 }
