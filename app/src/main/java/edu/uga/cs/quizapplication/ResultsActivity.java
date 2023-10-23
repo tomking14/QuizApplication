2e13f1bd399199c4fde657e70a94c326c8090a41
tomking14 <tomykastor@gmail.com>
2023-10-23 17:06:32 -0400

    buttons for each page

--- app/src/main/java/edu/uga/cs/quizapplication/ResultsActivity.java
+++ app/src/main/java/edu/uga/cs/quizapplication/ResultsActivity.java
@@ -15,15 +15,21 @@ import android.widget.TextView;
 public class ResultsActivity extends AppCompatActivity {
     private Button homeBtn;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.results_fragment); // Set the content view first
+
         homeBtn = findViewById(R.id.backToMainButton);
-        setContentView(R.layout.results_fragment); // Set the layout for the result screen
 
-
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMainActivity();
+            }
+        });
     }
+
     public void goToMainActivity() {
         Intent intent = new Intent(ResultsActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
