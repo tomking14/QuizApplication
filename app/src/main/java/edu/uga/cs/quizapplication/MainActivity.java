31ea1be2b06aa02fe0f9d661843f55a5e1f27e25
tomking14 <tomykastor@gmail.com>
2023-10-23 13:09:14 -0400

    adding stuff

--- app/src/main/java/edu/uga/cs/quizapplication/MainActivity.java
+++ app/src/main/java/edu/uga/cs/quizapplication/MainActivity.java
@@ -2,10 +2,16 @@ package edu.uga.cs.quizapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,25 @@ public class MainActivity extends AppCompatActivity {
             // Load the portrait layout (activity_child.xml)
             setContentView(R.layout.activity_main);
         }
+        startButton = findViewById(R.id.startBtn);
+        startButton.setOnClickListener( new overviewButtonClickListener());
 
+    }
+
+    private class overviewButtonClickListener implements View.OnClickListener
+    {
+        @Override
+        public void onClick( View view ) {
+            try {
+                // Gain access to the app's resources
+                Intent intent = new Intent( view.getContext(), ChildActivity.class );
+                startActivity( intent );
+            } catch (Exception e) {
+                // e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Uh oh, something went wrong...", Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
     }
 }
